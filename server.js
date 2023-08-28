836d2c18896d1f4889d6ff124fa4bdced0d69b28
Brennen-Fisher <brennen_fisher@outlook.com>
2023-08-28 10:51:15 -0500

    Added build

--- server.js
+++ server.js
@@ -5,16 +5,23 @@ import authRoute from "./routes/auth.route.js";
 import listRoute from "./routes/list.route.js";
 import bcrypt from "bcrypt";
 import cors from "cors";
+import path from "path";
 import cookieParser from "cookie-parser";   
 import {connectionString } from "./mongo.js";
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 app.use(cors({ origin: "http://localhost:5173", credentials: true}));
 app.use(express.json());
 app.use(cookieParser());
+// app.use(express.static(path.join("./dist")));
 mongoose.set('strictQuery', true);
+
+
 
 const connect = async () => {
     try {
@@ -25,6 +32,12 @@ const connect = async () => {
         console.log(error);
     }
 }
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 app.use("/api/user",userRoute);
 app.use("/api/auth",authRoute);
